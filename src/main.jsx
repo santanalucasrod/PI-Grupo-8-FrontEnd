70dc1995b6e6749dee82f009a860acc2a3ad42e4
unknown <lucas.kawakami@sptech.school>
2026-05-26 22:49:50 -0300

    fix:componentizando header

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,9 @@ import Login from './pages/Login.jsx'
 import CadastroFuncionario from './pages/CadastroFuncionario.jsx'
 import './index.css'
 import App from './App.jsx'
-import Header from './components/Header.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Header />
-    <CadastroFuncionario />
+    <Login />
   </StrictMode>,
 )
