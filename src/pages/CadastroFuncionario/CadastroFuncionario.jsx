ba9aed546a1d0911dd262e2ffc85c6fd6c0e4b0e
hilukaz <lucas.kawakami@sptech.school>
2026-08-16 11:54:53 -0300

    feat: importando axios

--- src/pages/CadastroFuncionario/CadastroFuncionario.jsx
+++ src/pages/CadastroFuncionario/CadastroFuncionario.jsx
@@ -1,4 +1,5 @@
 import styles from './CadastroFuncionario.module.css';
+import axios from 'axios';
 import { useEffect, useState } from "react";
 
 function CadastroFuncionario() {
@@ -31,46 +32,26 @@ function CadastroFuncionario() {
         console.log("FORM LOGIN: ", email);
         console.log("FORM SENHA: ", senha);
 
-        fetch("http://localhost:8080/funcionario/cadastro", {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify({
-                nome: nome,
-                email: email,
-                senha: senha,
-                gerente: false
-            })
+        axios.post("http://localhost:8080/funcionario/cadastro", {
+            nome: nome,
+            email: email,
+            senha: senha,
+            gerente: false
+
         }).then(function (resposta) {
-            console.log("ESTOU NO THEN DO entrar()!")
+            console.log("ESTOU NO THEN DO CADASTRO!");
 
-            if (resposta.ok) {
-                console.log(resposta);
+            console.log(resposta);
+            console.log(resposta.data);
 
-                resposta.json().then(json => {
-                    console.log(json);
-                    console.log(JSON.stringify(json));
-                    localStorage.token = json.token;
+            localStorage.token = resposta.data.token;
 
-                    setErro("Funcionário cadastrado com sucesso!");
-                    setMostrarErro(true);
-                    setTimeout(function () {
-                        window.location = "./index.html";
-                    }, 1000); // apenas para exibir o loading
+            setErro("Funcionário cadastrado com sucesso!");
+            setMostrarErro(true);
 
-                });
-
-            } else {
-
-                setErro("Não foi possível cadastrar o funcionário.");
-                setMostrarErro(true);
-
-                setTimeout(() => {
-                    setMostrarErro(false);
-                    setErro("");
-                }, 5000);
-            }
+            setTimeout(function () {
+                window.location = "./index.html";
+            }, 1000);
 
         }).catch(function (erro) {
             console.log(erro);
@@ -83,8 +64,6 @@ function CadastroFuncionario() {
                 setErro("");
             }, 5000);
         })
-
-        return false;
 
     }
 
